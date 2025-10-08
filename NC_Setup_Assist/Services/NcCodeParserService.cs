4d9bfefdbf14ab9b6a2b7f1f15ca911783b2ae42
Daniel Martinez <d.martinez@mail.ch>
2025-10-08 16:59:57 +0700

    0.5.2

--- NC_Setup_Assist/Services/NcCodeParserService.cs
+++ NC_Setup_Assist/Services/NcCodeParserService.cs
@@ -22,6 +22,7 @@ namespace NC_Setup_Assist.Services
             int reihenfolgeCounter = 1;
 
             int maximaleAbstechdrehzahl = 0;
+            int xValue = 0;
             string abstechwerkzeugStangenanfang = string.Empty;
             string anschlagWerkzeug = string.Empty;
             double abstechPositionZ = 0;
@@ -41,6 +42,9 @@ namespace NC_Setup_Assist.Services
             var korbZurückRegex = new Regex(@"\bM76\b");
             var toolRegex = new Regex(@"\bT(\d{2})(\d{2})?");
             var natRegex = new Regex(@"NAT(\d+)");
+            var xValueRegex = new Regex(@"X(\d+)", RegexOptions.IgnoreCase);
+            var g71Regex = new Regex(@"G71");
+            var steigungRegex = new Regex(@"F(\d +)");
             #endregion
 
             foreach (var line in lines)
@@ -132,6 +136,33 @@ namespace NC_Setup_Assist.Services
                 if (natMatch.Success)
                 {
                     nat = true;
+                }
+
+                var g71Match = g71Regex.Match(line);
+                if (g71Match.Success)
+                {
+                    var steigungMatch = steigungRegex.Match(line);
+                    if (steigungMatch.Success)
+                    {
+                        var xValueMatchg71 = xValueRegex.Match(line);
+                        if (xValueMatchg71.Success)
+                        {
+                            if (xValue > int.Parse(xValueMatchg71.Groups[1].Value))
+                            {
+
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                    }
+                }
+
+                var xValueMatch = xValueRegex.Match(line);
+                if (xValueMatch.Success)
+                {
+                    xValue = int.Parse(xValueMatch.Groups[1].Value);
                 }
 
                 // --- ZULETZT DIE WERKZEUGVERARBEITUNG ---
